ae08c1f9b7f1e3d144824ea59125fa8c35b4957b
GlennKim <gunhee21@gmail.com>
2024-10-28 23:43:52 +0800

    a

--- controller/server.py
+++ controller/server.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from control_model import finished_model
 
@@ -22,4 +23,4 @@ def climate_api():
     }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
